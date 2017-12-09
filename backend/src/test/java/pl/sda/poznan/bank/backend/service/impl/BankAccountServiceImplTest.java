cd57839d1d4fa278a8a1292062e6587e06cb3913
SOWSKIN\Sowskin <filip.sosnowski88@gmail.com>
2017-12-09 12:13:18 +0100

    version 0.95

---
+++ backend/src/test/java/pl/sda/poznan/bank/backend/service/impl/BankAccountServiceImplTest.java
@@ -0,0 +1,24 @@
+package pl.sda.poznan.bank.backend.service.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BankAccountServiceImplTest {
+    @Test
+    public void payment() throws Exception {
+    }
+
+    @Test
+    public void payoff() throws Exception {
+    }
+
+    @Test
+    public void transfer() throws Exception {
+    }
+
+    @Test
+    public void getUserAccounts() throws Exception {
+    }
+
+}
