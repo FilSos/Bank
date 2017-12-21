c01a93637465c84d4f1bb6c48ff9a3843f0142c6
SOWSKIN\Sowskin <filip.sosnowski88@gmail.com>
2017-12-21 20:58:47 +0100

    adding mocks

---
+++ backend/src/main/java/pl/sda/poznan/bank/backend/dao/UserRepositoryDao.java
@@ -0,0 +1,4 @@
+package pl.sda.poznan.bank.backend.dao;
+
+public interface UserRepositoryDao {
+}
--- backend/src/test/java/pl/sda/poznan/bank/backend/service/impl/BankAccountServiceImplTest.java
+++ backend/src/test/java/pl/sda/poznan/bank/backend/service/impl/BankAccountServiceImplTest.java
@@ -1,15 +1,22 @@
 package pl.sda.poznan.bank.backend.service.impl;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
+import pl.sda.poznan.bank.backend.model.BankAccount;
 import pl.sda.poznan.bank.backend.repository.BankAccountRepository;
 import pl.sda.poznan.bank.backend.repository.HistoryRepository;
 import pl.sda.poznan.bank.backend.service.BankUserDetailsService;
 import pl.sda.poznan.bank.backend.web.viewmodel.PaymentAndPayoffVM;
 
+import javax.sql.DataSource;
+
 import static org.junit.Assert.*;
 
-public class BankAccountServiceImplTest {
+@RunWith(MockitoJUnitRunner.class)
+public class BankAccountServiceImplTest  {
 
     @Autowired
     private HistoryRepository historyRepository;
@@ -18,13 +25,19 @@ public class BankAccountServiceImplTest {
     @Autowired
     private BankUserDetailsService bankUserDetailsService;
 
+    @Mock
+    DataSource dataBase;
+
 
     @Test
     public void payment() throws Exception {
         PaymentAndPayoffVM viewModel = new PaymentAndPayoffVM("61109010140000071219812874",
                 400.0,"Info test");
-        bankAccountRepository.findByAccountNumber(viewModel.getSourceAccountNumber());
-        assertNotNull("Account must exists",viewModel);
+        BankAccount byAccountNumber = bankAccountRepository.findByAccountNumber(viewModel.getSourceAccountNumber());
+        double balance = byAccountNumber.getBalance();
+
+        assertEquals(800.0,400.0);
+
 
     }
 
